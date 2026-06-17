379c466a6d3a850bf1ad26a7de64d8ff722bedb2
swatishayna <swati308mmu@gmail.com>
2026-06-17 19:40:21 +0530

    logger class added

---
+++ logger/custom_logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+
+class CustomLogger:
+    def __init__(self):
+        logs_dir=os.path.join(os.getcwd(),"logs")
+        self.logs_dir = os.path.join(os.getcwd(), logs_dir)
+        os.makedirs(self.logs_dir, exist_ok=True)
+        
+        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        log_file_path = os.path.join(logs_dir, log_file)
+
+        logging.basicConfig(
+            filename=log_file_path,
+            format="[ %(asctime)s ] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s",
+            level=logging.INFO,
+        )
+        
+    def get_logger(Self, name = __file__):
+        return logging.getLogger(os.path.basename(name))
