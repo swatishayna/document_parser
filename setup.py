0e5a4d832955bcae5248270542c5434e3e53657d
swatishayna <swati308mmu@gmail.com>
2026-06-17 11:19:20 +0530

    changes in setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Document Portal",
+    author = "Swati Pandey",
+    version = "1.0",
+    packages= find_packages()
+    
+    
+)
