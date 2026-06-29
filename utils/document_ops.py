574a6dca808c907d6d9d9173951d16fc1234b3e7
swatishayna <swati308mmu@gmail.com>
2026-06-29 11:51:11 +0530

    new dev branch

--- utils/document_ops.py
+++ utils/document_ops.py
@@ -7,6 +7,7 @@ from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, Te
 
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import DocumentPortalException
+
 SUPPORTED_EXTENSIONS = {".pdf", ".docx", ".txt"}
 
 
